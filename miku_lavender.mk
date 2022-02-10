b637f1a63f55819e16e381bf56b94c564715847a
Canary233 <2096276316@qq.com>
2022-02-10 17:36:44 +0800

    lavender:Build gapps

--- miku_lavender.mk
+++ miku_lavender.mk
@@ -7,6 +7,9 @@
 # Miku
 MIKU_MASTER := Canary233
 
+# MIKU GAPPS
+MIKU_GAPPS := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
